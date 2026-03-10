53c073064d90601819a1aed752d89e5689e0abba
0And1Story <witherman233@gmail.com>
2026-03-10 08:52:09 +0800

    Update neovim deprecated configs

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,16 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- Change "show document information" keymap from K to <F4>
-      keys[#keys + 1] = { "K", false }
-      keys[#keys + 1] = { "<F4>", vim.lsp.buf.hover }
-    end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            -- Change "show document information" keymap from K to <F4>
+            { "K", false },
+            { "<F4>", vim.lsp.buf.hover },
+          },
+        },
+      },
+    },
   },
 }
